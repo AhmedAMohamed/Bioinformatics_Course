9a75e2d950828b24491147f7bb45791a2d181912
Ahmed Alaa <ahmedalaa3307023@gmail.com>
2016-03-26 01:44:11 +0200

    filling the DP matrix
    Signed-off-by: Ahmed Alaa <ahmedalaa3307023@gmail.com>

--- needlrman.py
+++ needlrman.py
@@ -1,9 +1,40 @@
 __author__ = 'AhmedA'
 
-seq1 = "AAGGCG"
-seq2 = "AA"
+# sequences definitions
+seq1 = "AAAAAA"
+seq2 = "AAAAAA"
+# parameters definitions
+match = 2
+misMatch = -1
+gap = -2
 
-matrix = [[0 for x in range(len(seq1)+1)] for x in range(len(seq2)+1)]
+# initializing the matrix to zero
+matrix = []
+for i in range(len(seq1)+1):
+    matrix.append([])
+    for j in range(len(seq2)+1):
+        matrix[i].append(0)
 
+# filling the score matrix
+i = 0
+while i < len(matrix):
+    j = 0
+    while j < len(matrix[i]):
+        if i == 0 and j == 0:
+            matrix[i][j] = 0
+        elif i == 0:
+            matrix[i][j] = matrix[i][j-1] + gap
+        elif j == 0:
+            matrix[i][j] = matrix[i-1][j] + gap
+        else:
+            diagonal = misMatch
+            if seq1[i-1] == seq2[j-1]:
+                diagonal = match
+            matrix[i][j] = max(matrix[i-1][j-1] + diagonal, matrix[i-1][j] + gap, matrix[i][j-1] + gap)
+        j += 1
+    i += 1
+maxScore = matrix[len(seq1)][len(seq2)]
+# back tracking
 
-print(matrix)
+for i in matrix:
+    print(i)
