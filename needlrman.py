9999e1ff28fedfe9d6c9f112f11c24d27bcceb42
Ahmed Alaa <ahmedalaa3307023@gmail.com>
2016-03-26 02:23:58 +0200

    Simple back tracking
    Signed-off-by: Ahmed Alaa <ahmedalaa3307023@gmail.com>

--- needlrman.py
+++ needlrman.py
@@ -1,8 +1,32 @@
 __author__ = 'AhmedA'
 
+
+def backTrack(matrix, seq1, seq2, currentI, currentJ, result, maxScore):
+    if currentI == 0 and currentJ == 0:
+        print("done")
+        return
+        #return result
+    if matrix[currentI-1][currentJ-1] <= matrix[currentI][currentJ]:
+        print("diagonal")
+        print(seq1[currentI-1],seq2[currentJ-1])
+        #result[0] = result[0]+seq1[currentI-1]
+        #result[1] = result[1]+seq2[currentJ-1]
+        backTrack(matrix, seq1, seq2, currentI-1, currentJ-1, result, matrix[currentI-1][currentJ-1])
+    elif matrix[currentI-1][currentJ] <= matrix[currentI][currentJ]:
+        print("-",seq2[currentJ-1])
+        #result[0] = result[0]+"-"
+        #result[1] = result[1]+seq2[currentJ]
+        backTrack(matrix, seq1, seq2, currentI-1, currentJ, result, matrix[currentI-1][currentJ])
+    elif matrix[currentI][currentJ-1] <= matrix[currentI][currentJ]:
+        print(seq1[currentI-1],"-")
+        #result[0] = result[0]+seq1[currentI-1]
+        #result[1] = result[1]+"-"
+        backTrack(matrix, seq1, seq2, currentI, currentJ-1, result, matrix[currentI-1][currentJ])
+
+
 # sequences definitions
-seq1 = "AAAAAA"
-seq2 = "AAAAAA"
+seq1 = "gaattcagtta"
+seq2 = "ggatcga"
 # parameters definitions
 match = 2
 misMatch = -1
@@ -23,9 +47,9 @@ while i < len(matrix):
         if i == 0 and j == 0:
             matrix[i][j] = 0
         elif i == 0:
-            matrix[i][j] = matrix[i][j-1] + gap
+            matrix[i][j] = 0#matrix[i][j-1] + gap
         elif j == 0:
-            matrix[i][j] = matrix[i-1][j] + gap
+            matrix[i][j] = 0#matrix[i-1][j] + gap
         else:
             diagonal = misMatch
             if seq1[i-1] == seq2[j-1]:
@@ -33,8 +57,9 @@ while i < len(matrix):
             matrix[i][j] = max(matrix[i-1][j-1] + diagonal, matrix[i-1][j] + gap, matrix[i][j-1] + gap)
         j += 1
     i += 1
-maxScore = matrix[len(seq1)][len(seq2)]
-# back tracking
-
 for i in matrix:
     print(i)
+maxScore = matrix[len(seq1)][len(seq2)]
+# back tracking
+result = ["",""]
+r = backTrack(matrix, seq1, seq2, len(seq1), len(seq2), result, maxScore)
